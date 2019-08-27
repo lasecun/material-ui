a76cb126542a2caea6eecfce79eb112858e66905
Juanjo Marti <juanjo.marti@colba.es>
2019-08-27 19:47:31 +0200

    Update Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import ContainedButtons from './component/button';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Examples for Material UI
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                <body>
+                <Route path="/about/" component={ContainedButtons}/>
+                </body>
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/buttons/">Buttons</Link>
+                        </li>
+                        <li>
+                            <Link to="/users/">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Route path="/buttons/" component={ContainedButtons} />
+            </div>
+        </Router>
+    );
+};
 
 export default App;
